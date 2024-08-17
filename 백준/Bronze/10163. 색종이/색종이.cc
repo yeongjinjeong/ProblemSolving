d418c88c2bfc851f5b23f8ae9180971b559ac724
정영진(Jeong Yeong-Jin) <134537573+yeongjinjeong@users.noreply.github.com>
2024-08-17 16:55:47 +0900

    [Bronze I] Title: 색종이, Time: 0 ms, Memory: 2020 KB, Score: 42 point -BaekjoonHub

---
+++ 백준/Bronze/10163. 색종이/색종이.cc
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <queue>
+#include <algorithm>
+using namespace std;
+
+
+
+int main(){
+    int arr[101][101]={0};
+    int N;
+
+    cin >> N;
+    for(int i=1;i<=N;i++){
+        int y,x;
+        int w,h;
+        cin >> x >>y >>w>>h;
+        for(int m=y;m<y+h;m++){
+            for(int n = x; n < x+w;n++){
+                arr[m][n] = i;
+            }
+        }
+    }
+    
+    vector<int>cnt;
+    cnt.resize(N+1,0);
+    for(int i=0;i<101;i++){
+        for(int j=0;j<101;j++){
+//            cout << arr[i][j] ;
+            if(arr[i][j]!=0){
+                cnt[arr[i][j]]+=1;
+            }
+        }
+//        cout << "\n";
+    }
+    for(int i=1;i<N+1;i++){
+        cout << cnt[i]<<"\n";
+    }
+    
+}
