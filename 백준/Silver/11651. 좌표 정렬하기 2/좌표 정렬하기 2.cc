23265810258a30c91a7fd4e5decb61a7faf72d87
정영진(Jeong Yeong-Jin) <134537573+yeongjinjeong@users.noreply.github.com>
2024-08-26 17:35:36 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 104 ms, Memory: 3572 KB -BaekjoonHub

---
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.cc
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <queue>
+#include <algorithm>
+
+using namespace std;
+
+struct Node {
+	int first, second;
+};
+bool cmp(Node left, Node right) {
+
+	if (left.second < right.second) return true;
+	if (left.second > right.second) return false;
+
+	if (left.first < right.first) return true;
+	if (left.first > right.first) return false;
+
+	return false;
+}
+int main() {
+
+	int N;
+	cin >> N;
+
+	vector<Node>arr;
+	for (int i = 0; i < N; i++) {
+		int x, y;
+		cin >> x >> y;
+		arr.push_back({ x,y });
+	}
+
+	sort(arr.begin(), arr.end(), cmp);
+
+	for (int i = 0; i < N; i++) {
+		cout << arr[i].first << ' ' << arr[i].second << "\n";
+	}
+}
