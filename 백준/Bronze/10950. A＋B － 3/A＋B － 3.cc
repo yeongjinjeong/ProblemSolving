49be73efa6080a85fd53006a6651c309364780ac
정영진(Jeong Yeong-Jin) <134537573+yeongjinjeong@users.noreply.github.com>
2024-09-17 09:01:05 +0900

    [Bronze V] Title: A+B - 3, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.cc
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int T;
+    cin >> T;
+    for(int tc=0;tc<T;tc++){
+        int a,b;
+        cin >> a>>b;
+        cout << a+b<<"\n";
+    }
+}
