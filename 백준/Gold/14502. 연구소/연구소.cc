f9cf3258ac6ed40b8ea6e37370c3467f6b2e62bc
정영진(Jeong Yeong-Jin) <134537573+yeongjinjeong@users.noreply.github.com>
2024-11-05 09:31:54 +0900

    [Gold IV] Title: 연구소, Time: 292 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.cc
+++ 백준/Gold/14502. 연구소/연구소.cc
@@ -1,78 +1,79 @@
 #include <iostream>
-#include <algorithm>
 #include <vector>
 #include <queue>
 using namespace std;
 
+struct Node {
+	int row;
+	int col;
+};
+
 int N, M;
-int ydir[4] = { 1,-1,0,0 };
-int xdir[4] = { 0,0,1,-1 };
-int arr[8][8] = { 0 };
-int minVirus;
+vector<vector<int>> arr;
+int dirr[] = { 0,0,1,-1 };
+int dirc[] = { 1,-1,0,0 };
+int ans;
 
-int cnt1 = 0;
-int cnt2 = 0;
-
-vector<pair<int, int>>virus;
-void bfs() {
-	queue<pair<int, int>>v;
-	int visited[8][8] = { 0 };
-	for (int i = 0; i < virus.size(); i++) {
-		v.push(virus[i]);
-	}
-	int cnt = cnt2;
-	while (!v.empty()) {
-		pair<int, int>now = v.front(); v.pop();
-		visited[now.first][now.second] = 1;
-		for (int i = 0; i < 4; i++) {
-			int nY = now.first + ydir[i];
-			int nX = now.second + xdir[i];
-			if (nY < 0 || nY >= N || nX < 0 || nX >= M) continue;
-			if (arr[nY][nX] == 1 || arr[nY][nX] == 2) continue;
-			if (visited[nY][nX] == 1) continue;
-			visited[nY][nX] = 1;
-			cnt++;
-			v.push({ nY,nX });
-		}
-		if (cnt > minVirus) break;
-	}
-	if (cnt < minVirus) minVirus = cnt;
-}
-void dfs(int level, int ind) {
-	if (level >= 3) {
-		bfs();
-		return;
-	}
-	for (int i = 0; i < N*M; i++) {
-		int y = i / M;
-		int x = i % M;
-		if (arr[y][x] == 2 || arr[y][x] == 1) continue;
-		arr[y][x] = 1;
-		dfs(level + 1, i);
-		arr[y][x] = 0;
-
-	}
-}
-
-int main() {
-	cin >> N >> M;
-	//minVirus = N * M;
+int infec(vector<vector<int>> kim) {
+	queue<Node> q;
 	for (int i = 0; i < N; i++) {
 		for (int j = 0; j < M; j++) {
-			cin >> arr[i][j];
-			if (arr[i][j] == 1) {
-				cnt1++;
-			}
-			if (arr[i][j] == 2) {
-				cnt2++;
-				virus.push_back({ i,j });
+			if (kim[i][j] == 2) {
+				q.push({ i,j });
 			}
 		}
 	}
-	minVirus = N * M-cnt1;
-	dfs(0, 0);
-	//cout << minVirus;
-	//cout << endl;
-	int res = N * M - (cnt1 + 3) - minVirus;
-	cout << res;
+	while (!q.empty()) {
+		Node now = q.front();
+		q.pop();
+		for (int dir = 0; dir < 4; dir++) {
+			int nrow = now.row + dirr[dir];
+			int ncol = now.col + dirc[dir];
+			if (nrow < 0 || nrow >= N || ncol < 0 || ncol >= M) continue;
+			if (kim[nrow][ncol]) continue;
+			kim[nrow][ncol] = 2;
+			q.push({ nrow, ncol });
+		}
+	}
+	int temp = 0;
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < M; j++) {
+			if (kim[i][j] == 0) temp++;
+		}
+	}
+	return temp;
+}
+
+void dfs(int now) {
+	if (now == 3) {
+		int temp = infec(arr);
+		if (ans < temp) ans = temp;
+		return;
+	}
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < M; j++) {
+			if (arr[i][j] == 1 || arr[i][j] == 2) continue;
+			arr[i][j] = 1;
+			dfs(now + 1);
+			arr[i][j] = 0;
+		}
+	}
+}
+
+int main() {	
+	cin >> N >> M;
+	for (int i = 0; i < N; i++) {
+		vector<int> v;
+		for (int j = 0; j < M; j++) {
+			int temp;
+			cin >> temp;
+			v.push_back(temp);
+		}
+		arr.push_back(v);
+	}
+	ans = 0;
+	dfs(0);
+	cout << ans;
+
+	return 0;
 }
