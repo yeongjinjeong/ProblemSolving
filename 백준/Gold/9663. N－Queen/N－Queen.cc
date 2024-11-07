8c5973614281ed7f9523018911f0dd98bb5b4df5
정영진(Jeong Yeong-Jin) <134537573+yeongjinjeong@users.noreply.github.com>
2024-11-08 08:39:08 +0900

    [Gold IV] Title: N-Queen, Time: 1852 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.cc
+++ 백준/Gold/9663. N－Queen/N－Queen.cc
@@ -1,96 +1,34 @@
 #include <iostream>
-#include <string>
-#include <algorithm>
-
+#include <vector>
 using namespace std;
 
-int group1[20][20]; //미리 Queen의 방향을 매핑하기 위한 용도
-				   //좌하 방향
-int n;
-int dat_col[40]; // 수직 - index: col, value: 사용여부
-int dat1[100]; //좌하 - index: 좌하, value: 사용여부
-int dat2[100]; //우하 - index: 우하, value: 사용여부
+int N, ans;
+int visit1[15];
+int visit2[30];
+int visit3[30];
 
-
-void make_group1(int r, int c) {
-	int R = r;
-	int C = c;
-	while (1) {
-		if (R >= n || C < 0) return;
-		group1[R][C] = R + C;
-		R += 1;
-		C -= 1;
-	}
-}
-int group2[20][20]; //미리 Queen의 방향을 매핑하기 위한 용도
-					//우하 방향
-
-void make_group2(int r, int c) {
-	int R = r;
-	int C = c;
-	while (1) {
-		if (R >= n || C < 0) return;
-		group2[R][C] = R - C + n;
-		R += 1;
-		C -= 1;
-	}
-}
-
-int answer=0;
-
-void func(int row) {
-	if (row >= n) {
-		answer++;
+void dfs(int now) {
+	if (now == N) {
+		ans++;
 		return;
 	}
-	for (int col = 0; col < n; col++) {
-		
-		if (dat_col[col] == 1 || dat1[group1[row][col]] == 1 || dat2[group2[row][col]] == 1)
-			continue;
-		dat_col[col] = 1;
-		dat1[group1[row][col]] = 1;
-		dat2[group2[row][col]] = 1;
-
-		func(row + 1);
-		dat_col[col] = 0;
-		dat1[group1[row][col]] = 0;
-		dat2[group2[row][col]] = 0;
+	for (int i = 0; i < N; i++) {
+		if (visit1[i] == 1 || visit2[i + now] == 1 || visit3[now - i + N - 1] == 1) continue;
+		visit1[i] = 1;
+		visit2[i + now] = 1;
+		visit3[now - i + N - 1] = 1;
+		dfs(now + 1);
+		visit1[i] = 0;
+		visit2[i + now] = 0;
+		visit3[now - i + N - 1] = 0;
 	}
 }
 
 int main() {
-	cin >> n;
-
-	for(int r=0;r<n;r++){
-		for (int c = 0; c < n; c++) {
-			if (group1[r][c] == 0) {
-				make_group1(r, c);
-			}
-		}
-	}
-	/*for (int i = 0; i < n; i++) {
-		for (int j = 0; j < n; j++) {
-			cout << group1[i][j];
-		}
-		cout << endl;
-	}
-	cout << endl;*/
-	for (int r = 0; r < n; r++) {
-		for (int c = 0; c < n; c++) {
-			if (group2[r][c] == 0) {
-				make_group2(r, c);
-			}
-		}
-	}
-	/*for (int i = 0; i < n; i++) {
-		for (int j = 0; j < n; j++) {
-			cout << group2[i][j];
-		}
-		cout << endl;
-	}*/
-	func(0);
-
-	cout << answer;
+	cin >> N;	
+	ans = 0;
+	dfs(0);
+	cout << ans;
 
 	return 0;
 }
