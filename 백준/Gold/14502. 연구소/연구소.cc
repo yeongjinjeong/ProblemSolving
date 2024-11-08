3f77a925642370c130382b7d98c553ebfc6dfb65
정영진(Jeong Yeong-Jin) <134537573+yeongjinjeong@users.noreply.github.com>
2024-11-08 13:52:16 +0900

    [Gold IV] Title: 연구소, Time: 56 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.cc
+++ 백준/Gold/14502. 연구소/연구소.cc
@@ -10,18 +10,27 @@ struct Node {
 
 int N, M;
 vector<vector<int>> arr;
+vector<Node> zero;
+vector<int> visited;
+vector<int> path;
 int dirr[] = { 0,0,1,-1 };
 int dirc[] = { 1,-1,0,0 };
 int ans;
 
-int infec(vector<vector<int>> kim) {
+int infec() {
 	queue<Node> q;
 	for (int i = 0; i < N; i++) {
 		for (int j = 0; j < M; j++) {
-			if (kim[i][j] == 2) {
+			if (arr[i][j] == 2) {
 				q.push({ i,j });
 			}
 		}
+	}
+	vector<vector<int>> kim = arr;
+	for (int i = 0; i < 3; i++) {
+		int row = zero[path[i]].row;
+		int col = zero[path[i]].col;
+		kim[row][col] = 1;
 	}
 	while (!q.empty()) {
 		Node now = q.front();
@@ -46,32 +55,37 @@ int infec(vector<vector<int>> kim) {
 
 void dfs(int now) {
 	if (now == 3) {
-		int temp = infec(arr);
+		int temp = infec();
 		if (ans < temp) ans = temp;
 		return;
 	}
-	for (int i = 0; i < N; i++) {
-		for (int j = 0; j < M; j++) {
-			if (arr[i][j] == 1 || arr[i][j] == 2) continue;
-			arr[i][j] = 1;
-			dfs(now + 1);
-			arr[i][j] = 0;
-		}
+	for (int i = 0; i < zero.size(); i++) {
+		if (visited[i]) continue;
+		if (now > 0 && i < path[now - 1]) continue;
+		path.push_back(i);
+		visited[i] = 1;
+		dfs(now + 1);
+		visited[i] = 0;
+		path.pop_back();				
 	}
 }
 
-int main() {	
+int main() {
 	cin >> N >> M;
 	for (int i = 0; i < N; i++) {
 		vector<int> v;
 		for (int j = 0; j < M; j++) {
 			int temp;
 			cin >> temp;
+			if (temp == 0) zero.push_back({ i,j });
 			v.push_back(temp);
 		}
 		arr.push_back(v);
 	}
 	ans = 0;
+	for (int i = 0; i < zero.size(); i++) {
+		visited.push_back(0);
+	}
 	dfs(0);
 	cout << ans;
 
