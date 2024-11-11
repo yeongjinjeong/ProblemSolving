49641a0330985581ff08e324de8361351493b50c
정영진(Jeong Yeong-Jin) <134537573+yeongjinjeong@users.noreply.github.com>
2024-11-11 14:06:12 +0900

    [Gold V] Title: 토마토, Time: 76 ms, Memory: 11184 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.cc
+++ 백준/Gold/7576. 토마토/토마토.cc
@@ -1,57 +1,55 @@
 #include <iostream>
-#include <vector>
 #include <queue>
-
 using namespace std;
-int zero = 0;
-int N, M;
-vector<vector<int>>tomato;
-int ydir[4] = { 1,-1,0,0 };
-int xdir[4] = { 0,0,1,-1 };
-struct Next {
-	int m;
-	int n;
-	int level;
-};
-queue<Next>q;
-int cnt=0;
-void bfs() {
-	
-	while (!q.empty()) {
-		Next now = q.front();
-		q.pop();
-		cnt = now.level;
-		for (int i = 0; i < 4; i++) {
-			int y = now.m + ydir[i];
-			int x = now.n + xdir[i];
-			if (y < 0 || x < 0 || x >= N || y >= M) continue;
-			if (tomato[y][x] == -1 || tomato[y][x]==1) continue;
-			q.push({ y,x, cnt+1 });
-			zero--;
-			tomato[y][x] = 1;
-		}
-	}
 
-}
+int N, M, ans;
+int arr[1000][1000];
+bool visited[1000][1000];
+int dirr[] = { -1,1,0,0 };
+int dirc[] = { 0,0,-1,1 };
+
 int main() {
-	cin >> N >> M;
-	for (int i = 0; i < M; i++) {
-		tomato.push_back(vector<int>(N));
-		for (int j = 0; j < N; j++) {
-			cin >> tomato[i][j];
-			if (tomato[i][j] == 0) zero++;
-			if (tomato[i][j] == 1) q.push({ i,j,0 });
+	ios::sync_with_stdio(false);
+	cin.tie(0); cout.tie(0);
+	cin >> M >> N;
+	queue<pair<int, int>> q;
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < M; j++) {
+			cin >> arr[i][j];
+			if (arr[i][j] == 1) {
+				visited[i][j] = true;
+				q.push({ i,j });				
+			}
 		}
 	}
-	if (zero == 0) {
-		cout << 0;
-		return 0;
+	while (1) {
+		int dongi = q.size();
+		for (int i = 0; i < dongi; i++) {
+			int nowrow = q.front().first;
+			int nowcol = q.front().second;
+			q.pop();
+			for (int dir = 0; dir < 4; dir++) {
+				int nrow = nowrow + dirr[dir];
+				int ncol = nowcol + dirc[dir];
+				if (nrow < 0 || ncol < 0 || nrow >= N || ncol >= M) continue;
+				if (visited[nrow][ncol]) continue;
+				if (arr[nrow][ncol] != 0) continue;
+				arr[nrow][ncol] = 1;
+				visited[nrow][ncol] = true;
+				q.push({ nrow, ncol });
+			}
+		}
+		ans++;
+		if (q.empty()) break;
 	}
-	else {
-		bfs();
-		if (zero == 0) cout << cnt;
-		else cout << -1;
+	bool flag = false;
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < M; j++) {
+			if (arr[i][j] == 0) flag = true;
+		}
+	}
+	if (flag) cout << -1;
+	else cout << ans - 1;
 
-		return 0;
-	}
+	return 0;
 }
