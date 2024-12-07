2d6080b0f4b92d0fdc844d730f6a9d1d83f65da3
정영진(Jeong Yeong-Jin) <134537573+yeongjinjeong@users.noreply.github.com>
2024-12-08 00:29:41 +0900

    [level 0] Title: 홀수 vs 짝수, Time: 0.01 ms, Memory: 4.15 MB -BaekjoonHub

---
+++ 프로그래머스/0/181887. 홀수 vs 짝수/홀수 vs 짝수.cpp
@@ -0,0 +1,20 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> num_list) {
+    int answer = 0;
+    int sum1=0,sum2=0;
+    for(int i=0; i< num_list.size();i++){
+        if(i%2==0){
+            sum1+=num_list[i];
+        }
+        else{
+            sum2+=num_list[i];
+        }
+        
+    }
+    answer = max(sum1,sum2);
+    return answer;
+}
