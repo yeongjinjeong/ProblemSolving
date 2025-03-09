ba8a00d9de28461a56b444a68065686577ace7d0
정영진(Jeong Yeong-Jin) <134537573+yeongjinjeong@users.noreply.github.com>
2025-03-09 13:45:48 +0900

    [Silver IV] Title: 수 찾기, Time: 60 ms, Memory: 6820 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.cc
+++ 백준/Silver/1920. 수 찾기/수 찾기.cc
@@ -1,42 +1,29 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
+#include<iostream>
+#include<unordered_set>
+
 using namespace std;
 
-int N, M;
-
-int main()
-{
+int main() {
 	ios::sync_with_stdio(false);
-	cin.tie(0); cout.tie(0);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	int N, M;
+	unordered_set<int>s;
 	cin >> N;
-	vector<int> v;
-	for (int i = 0; i < N; i++)
-	{
-		int temp;
-		cin >> temp;
-		v.push_back(temp);
+	for (int i = 0; i < N; i++) {
+		int num;
+		cin >> num;
+		s.insert(num);
 	}
-	sort(v.begin(), v.end());
+
 	cin >> M;
-	for (int i = 0; i < M; i++)
-	{
-		int target;
-		cin >> target;
-		int left = 0;
-		int right = N - 1;
-		while (left <= right)
-		{
-			int mid = (left + right) / 2;
-			if (v[mid] == target)
-			{
-				cout << 1 << "\n";
-				break;
-			}
-			if (v[mid] < target) left = mid + 1;
-			else right = mid - 1;
+	for (int i = 0; i < M; i++) {
+		int num;
+		cin >> num;
+		if (s.find(num) == s.end()) {
+			cout << 0 << "\n";
 		}
-		if (right < left) cout << 0 << "\n";
+		else cout << 1 << "\n";
 	}
-	return 0;
 }
