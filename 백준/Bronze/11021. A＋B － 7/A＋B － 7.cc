7fbc1ec9301a32778ea4f0858128f367d81d25ce
정영진(Jeong Yeong-Jin) <134537573+yeongjinjeong@users.noreply.github.com>
2025-07-21 11:19:37 +0900

    [Bronze V] Title: A+B - 7, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.cc
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    
+    int T;
+    cin >> T;
+    
+    for(int i=1;i<=T;i++){
+        int a, b;
+        cin >> a >> b;
+        cout << "Case #" << i << ": " << (a+b) << "\n";
+    }
+}
