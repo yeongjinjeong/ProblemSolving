a9666a70b63fe0367da0ed160ca8b30f50fe9ebd
정영진(Jeong Yeong-Jin) <134537573+yeongjinjeong@users.noreply.github.com>
2025-09-10 09:53:04 +0900

    Time: 0 ms (100%), Space: 8.6 MB (31.45%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.c
+++ 0007-reverse-integer/0007-reverse-integer.c
@@ -4,7 +4,10 @@ int reverse(int x){
     long long answer = 0;
     int num = x;
     while(num!=0){
+        //int의 범위가 넘어가는 경우는 예외 처리
         if(answer*10 >= INT_MAX || answer*10 <= INT_MIN) return 0;
+        
+        //이전 값에 10을 곱해주며 다음 숫자를 더해준다.
         answer *= 10;
         int rest = num%10;
         answer += rest;
