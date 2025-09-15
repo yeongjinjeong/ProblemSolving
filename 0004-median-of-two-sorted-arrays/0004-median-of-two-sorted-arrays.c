f4226b6b4011cbf837630712c7c1b2f889da9d81
정영진(Jeong Yeong-Jin) <134537573+yeongjinjeong@users.noreply.github.com>
2025-09-15 09:22:47 +0900

    Time: 0 ms (100%), Space: 11.5 MB (24.47%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.c
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.c
@@ -1,6 +1,7 @@
 double findMedianSortedArrays(int* nums1, int nums1Size, int* nums2, int nums2Size) {
     int *new_arr = (int*)malloc((nums1Size + nums2Size) * sizeof(int));
 
+    //nums1 에  아무런 배열도 없는 경우
     if(nums1Size==0){
         int median_ind = nums2Size/2;
         double median;
@@ -14,6 +15,7 @@ double findMedianSortedArrays(int* nums1, int nums1Size, int* nums2, int nums2Si
         return median;
     }
 
+    //nums2 에 아무런 배열도 없는 경우
     if(nums2Size==0){
         int median_ind = nums1Size/2;
         double median;
@@ -26,14 +28,18 @@ double findMedianSortedArrays(int* nums1, int nums1Size, int* nums2, int nums2Si
 
         return median;
     }
-
+    //nums1,2 배열에 접근하는 인덱스
     int ind1=0,ind2=0;
-    int finish1 = 0;
-    int finish2 = 0;
 
+    //nums1,2 배열의 인덱스가 마지막 요소까지 도달했는지 확인하는 플래그 
+    int finish1 = 0, finish2 = 0;
+
+    //median 인덱스의 위치
     int median_ind = (nums1Size + nums2Size)/2;
 
-    for(int i=0;i<median_ind+1;i++){
+    //median 위치까지만 반복
+    for(int i=0;i<nums1Size + nums2Size+1;i++){
+        //nums1 과 num2의 값을 비교하며 merge 한다.
         if(finish1 == 0 && ( ind2 >= nums2Size || nums1[ind1] <= nums2[ind2] )){
             new_arr[i] = nums1[ind1];
             ind1++;
