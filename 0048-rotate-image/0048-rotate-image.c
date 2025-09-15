27ca608d62c50f3bc0fe5229db5796adb683efec
정영진(Jeong Yeong-Jin) <134537573+yeongjinjeong@users.noreply.github.com>
2025-09-15 16:34:32 +0900

    Time: 0 ms (100%), Space: 10.2 MB (97.88%) - LeetHub

---
+++ 0048-rotate-image/0048-rotate-image.c
@@ -0,0 +1,19 @@
+void rotate(int** matrix, int matrixSize, int* matrixColSize) {
+    for(int i=0;i<matrixSize/2;i++){
+        for(int j=i;j<matrixSize-i-1;j++){
+            int y= i, x = j;
+            int pre = matrix[y][x];
+            int next;
+            for(int k=0;k<4;k++){
+                next = matrix[x][matrixSize-1-y];
+                matrix[x][matrixSize-1-y] = pre;
+                pre = next;
+                int ny = x;
+                int nx = matrixSize-1-y;
+                y = ny;
+                x = nx; 
+            }
+        }
+    }
+
+}
